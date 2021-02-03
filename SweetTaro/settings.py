519540520726ea33ceb087583ef6858bd8d2b062
damien.yang <damien.yang@zkteco.com>
2021-02-03 14:23:04 +0800

    add data path

--- SweetTaro/settings.py
+++ SweetTaro/settings.py
@@ -141,6 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FILE_URL = '/files/'
+FILE_ROOT = os.path.join(BASE_DIR, 'data')
 
 # django-rest-framework setting
 REST_FRAMEWORK = {
